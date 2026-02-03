f641991ded6305d5714321129fbc5c0929415859
chanjin346 <ckswls2231@gmail.com>
2026-02-03 10:38:43 +0900

    commit

---
+++ 06_Parkchanjin/chap01-rest-api-lecture-source/src/main/java/com/google/restapi/section01/response/Message.java
@@ -0,0 +1,14 @@
+package com.google.restapi.section01.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class Message {
+  private int httpStatusCode;
+  private String message;
+
+}
